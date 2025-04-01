9cc88ace7735b398a578dd4fb86164b66159ef8d
Julio <santosjco@yahoo.com.br>
2025-04-01 13:28:01 -0300

    pausa para ir para API

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-from recipes.views import home, contato, sobre
+from recipes.views import home
+from . import views
 
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
-    path('contato/', contato),
-    path('sobre/', sobre)
+    path('', views.home, name="home"), #Home
+    path('recipes/category/<int:category_id>/', views.category, name="category"), 
+    path('recipes/<int:id>/', views.recipe, name="recipe"), #Home    
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,36 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404
+from utils.recipes.factory import make_recipe
+from .models import Recipe
+from django.http import Http404
+
+from recipes.models import Recipe
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
-        'name': 'Júlio Cesar', 'age': '36',
+    recipes = Recipe.objects.filter(
+            is_published=True
+        ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
     })
 
-def contato(request):
-    return render(request, 'global/home.html')
+def category(request, category_id):
+    recipes = get_list_or_404( Recipe.objects.filter(
+        category__id=category_id,
+        is_published = True
+        ).order_by('-id')
+    )
+    
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name} - Category |'
+    })
 
-def sobre(request):
-    return HttpResponse('SOBRE')
+def recipe(request, id):     
+     recipe = Recipe.objects.filter(
+         pk=id,
+         is_published=True,
+     ).order_by('-id').first()     
+     return render(request, 'recipes/pages/recipe-view.html', context={
+         'recipe': recipe,
+         'is_detail_page': True,
+     })
